288fff2f217fa9ebf5a5907982bff5a50b2f647f
TheGreatMisconception <114302667+TheGreatMisconception@users.noreply.github.com>
2022-11-17 13:56:54 +0100

    remove try catch

--- CheckSSLCertExpiration/src/certificate/certificate.py
+++ CheckSSLCertExpiration/src/certificate/certificate.py
@@ -12,20 +12,17 @@ class Certificate():
         self.regex = regex
        
     def check_expiration_date(self):
-        try:
-            context = ssl.create_default_context()
-            with socket.create_connection((self.hostname, self.port)) as sock:
-                with context.wrap_socket(sock, server_hostname=self.hostname) as ssock:
-                    self.certificate = ssock.getpeercert()
-                    self.notAfter_string = self.certificate["notAfter"]
-                    self.notBefore_string = self.certificate["notBefore"]
-            self.notBefore = datetime.strptime(self.notBefore_string, self.regex)
-            self.notAfter = datetime.strptime(self.notAfter_string, self.regex)
-            self.last_check_date = datetime.now()
-            self.is_valid = self.notAfter > datetime.now()
-            return self.is_valid
-        except:
-            return "[Error] Cant check SSL Cert"
+        context = ssl.create_default_context()
+        with socket.create_connection((self.hostname, self.port)) as sock:
+            with context.wrap_socket(sock, server_hostname=self.hostname) as ssock:
+                self.certificate = ssock.getpeercert()
+                self.notAfter_string = self.certificate["notAfter"]
+                self.notBefore_string = self.certificate["notBefore"]
+        self.notBefore = datetime.strptime(self.notBefore_string, self.regex)
+        self.notAfter = datetime.strptime(self.notAfter_string, self.regex)
+        self.last_check_date = datetime.now()
+        self.is_valid = self.notAfter > datetime.now()
+        return self.is_valid
                 
                 
     def get_notAfter(self):

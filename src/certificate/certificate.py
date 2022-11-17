5843450d781d35f15c6c23f24ba78cbea40115c0
TheGreatMisconception <114302667+TheGreatMisconception@users.noreply.github.com>
2022-11-17 13:45:24 +0100

    Delete Src

--- src/certificate/certificate.py
+++
@@ -1,4 +0,0 @@
-
-class Certificate():
-    def __init__(self):
-        pass
--- src/smtp/smtp.py
+++
@@ -1,7 +0,0 @@
-import smtplib, ssl
-
-class EMAIL():
-    def __init__(self, port: int, hostname: str, password: str):
-        self.port = port
-        self.hostname = hostname
-        self.password = password
